07d7367b6a0024ac785c514c0034f7e9b23f6600
Bilal Abdullah <abdullahbilal88@gmail.com>
2017-04-19 14:50:19 +0300

    Merged with master

--- Demo/app/src/main/java/ee/ut/demo/injector/component/FragmentComponent.java
+++ Demo/app/src/main/java/ee/ut/demo/injector/component/FragmentComponent.java
@@ -3,11 +3,8 @@ package ee.ut.demo.injector.component;
 import android.content.Context;
 
 import dagger.Component;
-import ee.ut.demo.activity.EventsActivity;
-import ee.ut.demo.fragment.EventFragment;
 import ee.ut.demo.fragment.HomeFragment;
 import ee.ut.demo.injector.module.ActivityModule;
-import ee.ut.demo.injector.module.EventsModule;
 import ee.ut.demo.injector.module.FragmentModule;
 import ee.ut.demo.injector.scope.PerActivity;
 
